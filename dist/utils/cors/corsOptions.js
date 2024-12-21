5cc15951b824cdbf873e90b5975d05796bb273ca
Afuh Flyine Tembeng <flyinnsafuh@gmail.com>
2024-12-21 15:02:53 +0100

    Update corsOptions.js

--- dist/utils/cors/corsOptions.js
+++ dist/utils/cors/corsOptions.js
@@ -1,8 +1,4 @@
-import { config } from "dotenv";
-config();
-const allowedOrigins = [
-    process.env.CLIENT_URL,
-    process.env.EXTERNAL_CLIENT_URL,
-];
-export default allowedOrigins;
+
+const allowedOrigin = "https://syntax-springdev.vercel.app/";
+export default allowedOrigin;
 //# sourceMappingURL=corsOptions.js.map
